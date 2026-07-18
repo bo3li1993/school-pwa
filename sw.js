62d5b3c48607cf7b33027b9a680864ca9c79fd58
bo3li1993 <bo3li1993@gmail.com>
2026-07-18 05:48:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -40,6 +40,7 @@ const ASSETS_TO_CACHE = [
     './tv.html',
     './super.html',
     './import.html',
+    './department_head.html',
     'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
     'https://fonts.googleapis.com/css2?family=Cairo:wght=400;600;700;900&display=swap'
 ];
